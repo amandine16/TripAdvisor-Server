b03ab47b51d6b21f63d55c34dc8b964ce587b104
Amandine <amandinedelavoie@live.fr>
2021-01-31 14:15:39 +0100

    version locale

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(formidable());
 app.use(cors());
 
-//MailGun configuration
+// //MailGun configuration
 const api_key = process.env.MAIL_GUN_API_KEY;
 const domain = process.env.MAILGUN_DOMAIN;
 const mailgun = require("mailgun-js")({ apiKey: api_key, domain: domain });
@@ -16,14 +16,14 @@ const mailgun = require("mailgun-js")({ apiKey: api_key, domain: domain });
 //Route en get
 app.get("/", async (req, res) => {
   try {
-    res.status("route get");
+    res.status(200).json("route get");
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
 });
 
 //Route en post
-app.post("/", async (req, res) => {
+app.post("/", (req, res) => {
   //Destructuring
   console.log(req.fields);
   const { firstname, lastname, email, subject, message } = req.fields;
@@ -39,7 +39,7 @@ app.post("/", async (req, res) => {
   mailgun.messages().send(data, (error, body) => {
     if (!error) {
       console.log(data);
-      return res.json(body);
+      return res.status(200).json(body);
     } else {
       console.log(error);
       res.status(401).json(error);
